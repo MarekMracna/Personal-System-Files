0683e4983575715d82a1bb1d6686c1254b2f6d35
Marek Mracna <marek.mracna07@gmail.com>
2018-07-26 22:07:04 +0200

    Refactoring and error handling

--- GlobalNodeModules/Server/src/index.js
+++ GlobalNodeModules/Server/src/index.js
@@ -1,29 +1,36 @@
 exports.run = (options) => {
-    const m_server = require('./server.js');
-    options.port = parseInt(options.port);
     try {
-        options = testArgs(options);
+        options = validateArgs(options);
     } catch (error) {
+        console.error(error.message);
+        printUsage();
         return;
     }
+    const m_server = require('./server.js');
     const Server = new m_server(options);
     Server.listen();
 }
 
-function testArgs(args) {
+function validateArgs(args) {
     let fixed = {};
-    if(require('./util.js').validPath(args.index)) {
+    const utils = require('./util.js');
+
+    // Index validation
+    if(utils.validPath(args.index))
         fixed.index = args.index;
-    } else {
-        console.error("\x1b[35mserve[arg:1]\x1b[0m Path is not valid");
-        throw Error("Path is not valid")
-    }
-    if(typeof(args.port) != "number" || isNaN(args.port)) {
-        console.error("\x1b[35mserve[arg:2]\x1b[0m Expected a Number, received a " + typeof(args.index));
-        console.log("Port set to \x1b[33m80\x1b[0m");
-        fixed.port = 80;
-    } else {
-        fixed.port = args.port;
-    }
+    else throw Error("\x1b[31mserve[arg:1]\x1b[0m Path is not valid");
+
+    // Port validation
+    if(utils.validPort(args.port))
+        fixed.port = parseInt(args.port);
+    else throw Error("\x1b[31mserve[arg:2]\x1b[0m Port is not valid");
+
     return fixed;
+}
+
+function printUsage() {
+    const usage = `Usage: serve <path> <port>
+    Where \x1b[33m<path>\x1b[0m: Path to served page directory (can be both absolute and relative)
+    And   \x1b[33m<port>\x1b[0m: Port for the server to use`
+    console.log(usage);
 }
--- GlobalNodeModules/Server/src/util.js
+++ GlobalNodeModules/Server/src/util.js
@@ -1,15 +1,19 @@
 module.exports.validPath = (path) => {
-    let endsSlash = /^.*(\\|\/)$/;
-    let absolute  = /^[A-Z]{1}:((\\|\/)\w*)+$/;
-    let relative  = /^(\.|(\.{2}))?((\\|\/)\w*)+$/;
+    const endsSlash = /^.*(\\|\/)$/;
+    const absolute  = /^[A-Z]{1}:((\\|\/)\w*)+$/;
+    const relative  = /^(\.|(\.{2}))?((\\|\/)\w*)+$/;
 
     if(endsSlash.test(path))
         path = path.slice(0,-1);
-    console.log(path);
     if(absolute.test(path) || relative.test(path)) {
         return pathExists(path);
     }
     return false;
+}
+
+module.exports.validPort = (port) => {
+    const isNumber = /^\d{1,5}$/;
+    return isNumber.test(port);
 }
 
 function pathExists(path) {
